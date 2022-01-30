b34faff39d9b7248e96ecd21a6b88ca4c1116d28
rum1r <3617306+rum1r@users.noreply.github.com>
2022-01-30 12:36:09 +0900

    52. Reactでの実装を意識したモックに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["uuu", "えええ"]);
   return (
     <>
       <div className="input-area">
@@ -10,20 +13,28 @@ export const App = () => {
       <div className="incomplete-area">
         <p>未完了のTODO</p>
         <ul>
-          <li className="list-row">
-            <p>ああああ</p>
-            <button>完了</button>
-            <button>削除</button>
-          </li>
+          {incompleteTodos.map((todo, index) => {
+            return (
+              <li key={index} className="list-row">
+                <p>{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p>完了のTODO</p>
         <ul>
-          <li className="list-row">
-            <p>うう</p>
-            <button>戻す</button>
-          </li>
+          {completeTodos.map((todo, index) => {
+            return (
+              <li key={index} className="list-row">
+                <p>{todo}</p>
+                <button>戻す</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
